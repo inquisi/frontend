b055799bf095cd032fa77d652f9dd8c21d0a0048
Nate Moore <ntmoore@knox.edu>
2015-04-02 16:00:55 -0500

    Remove bower components, add loginPanel stuff

--- app/controllers/test.js
+++
@@ -1,3 +0,0 @@
-function blah() {
-	console.log("blahsss");
-}
--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
 var concat = require('gulp-concat');
 var server = require('gulp-webserver');
+var less = require('gulp-less');
 
 var paths = {
-	'js': ['app/**/*.js', '!app/app.js']
+	'js': ['app/**/*.js', '!app/app.js'],
+	'less': ['app/less/**/*.less']
 }
 
 gulp.task('concat', function(done) {
 	gulp.src(paths.js)
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest('app'));
+	done();
+});
+
+gulp.task('less', function(done) {
+	gulp.src('app/less/app.less')
+		.pipe(less({
+			// paths: []
+		}))
+		.pipe(gulp.dest("app/"));
 	done();
 });
 
@@ -33,5 +44,6 @@ gulp.task('server', function() {
 
 gulp.task('default', function() {
 	gulp.watch(paths.js, ['concat']);
+	gulp.watch(paths.less, ['less']);
 	gulp.start('server');
 });
