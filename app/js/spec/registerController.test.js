d01fd150e87260412a84a9c0f87432347c8d70c3
Cody Sehl <cody.sehl@gmail.com>
2015-04-12 19:14:08 -0500

    fixing issue where user role wasn't being added on the register page

--- app/js/spec/registerController.test.js
+++ app/js/spec/registerController.test.js
@@ -1,21 +1,26 @@
 describe('registerController', function() {
 
-    var scope;
+	var scope;
 
-    beforeEach(function() {
-        module('inquisi');
-        inject(function($rootScope, $controller) {
-            scope = $rootScope.$new();
-            $controller("roleController", {
-                $scope: scope
-            });
-        });
-    });
+	beforeEach(function() {
+		module('inquisi');
+		inject(function($rootScope, $controller) {
+			scope = $rootScope.$new();
+			$controller("registerController", {
+				$scope: scope
+			});
+		});
+	});
 
-    // it("should call User.create ", function() {
-    //     expect(scope.user.role).not.toBe(null);
-    //     expect(scope.user.role).toBe("student");
-    // });
+	it("should set role to student by default", function() {
+		expect(scope.user.role).not.toBe(null);
+		expect(scope.user.role).toBe("student");
+	});
+
+	// it("should call User.create ", function() {
+	//     expect(scope.user.role).not.toBe(null);
+	//     expect(scope.user.role).toBe("student");
+	// });
 });
 
 // it('should catch invalid emails', function() {
