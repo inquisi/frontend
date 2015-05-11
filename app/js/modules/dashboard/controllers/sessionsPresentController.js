b113e9c3f1eff16606b7ffd671e04014404f1947
Nate Moore <ntmoore@knox.edu>
2015-05-11 16:59:06 -0500

    Presentation Mode WIP

--- app/js/modules/dashboard/controllers/sessionsPresentController.js
+++ app/js/modules/dashboard/controllers/sessionsPresentController.js
@@ -17,6 +17,51 @@ function sessionsPresentController($rootScope, $scope, $filter, focus, screenmat
         questionId: $scope.questions[0].id
     })
 
+    $rootScope.$on('$stateChangeSuccess',
+        function(event, toState, toParams, fromState, fromParams) {
+            var index = toParams.index;
+
+            if (index == 0) {
+                $scope.canPrevPresent = false;
+                if ($scope.questions.length - 1 == 0) {
+                    $scope.canNextPresent = false;
+                } else {
+                    $scope.canNextPresent = true;
+                }
+            } else if (index == $scope.questions.length - 1) {
+                $scope.canPrevPresent = true;
+                $scope.canNextPresent = false;
+            } else {
+                $scope.canPrevPresent = true;
+                $scope.canNextPresent = true;
+            }
+        });
+
+    $scope.prevQuestion = function() {
+        var index = $state.params.index;
+
+        if (index > 0) {
+            $state.go('questionsPresent', {
+                index: index - 1,
+                questionId: $scope.questions[index - 1].id
+            });
+        } else {
+            return
+        }
+    }
+
+    $scope.nextQuestion = function() {
+        var index = $state.params.index;
+        if (index < $scope.questions.length - 1) {
+            $state.go('questionsPresent', {
+                index: index + 1,
+                questionId: $scope.questions[index + 1].id
+            });
+        } else {
+            return
+        }
+    }
+
     $scope.toggleFS = function() {
         $scope.fsState = false;
 
