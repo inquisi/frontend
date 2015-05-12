5be7cc62cc2ab28e20f6c00cf84fdbf2de9212f0
Nate Moore <ntmoore@knox.edu>
2015-05-12 15:01:55 -0500

    Add empty input handling for login

--- app/js/modules/login/controllers/loginController.js
+++ app/js/modules/login/controllers/loginController.js
@@ -1,19 +1,31 @@
-function loginController($rootScope, $scope, $state, AuthService) {
+function loginController($rootScope, $scope, $state, AuthService, focus) {
     // function to submit the form after all validation has occurred            
     $scope.submit = function() {
-        AuthService.login($scope.user.email, $scope.user.password)
-            .then(function() {
-                if ($rootScope.redirectAfterLogin) {
-                    var redirectAfterLogin = $rootScope.redirectAfterLogin;
-                    delete $rootScope.redirectAfterLogin;
-                    $state.go(redirectAfterLogin);
-                } else {
-                    $state.go('dashboard.home');
-                }
-            }, function(data) {
-                $scope.errorMessage = data.message;
-            });
+        if ($scope.user == undefined) {
+            $scope.errorMessage = "Please enter your information";
+            focus('userEmail');
+        } else if ($scope.user.email == undefined) {
+            $scope.errorMessage = "Please enter an email";
+            focus('userEmail');
+        } else if ($scope.user.password == undefined) {
+            $scope.errorMessage = "Please enter a password";
+            focus('userPassword');
+        } else {
+            AuthService.login($scope.user.email, $scope.user.password)
+                .then(function() {
+                    if ($rootScope.redirectAfterLogin) {
+                        var redirectAfterLogin = $rootScope.redirectAfterLogin;
+                        delete $rootScope.redirectAfterLogin;
+                        $state.go(redirectAfterLogin);
+                    } else {
+                        $state.go('dashboard.home');
+                    }
+                }, function(data) {
+                    $scope.errorMessage = data.message;
+                    debugger
+                });
+        }
     }
 }
 
-login.controller('loginController', ['$rootScope', '$scope', '$state', 'AuthService', loginController]);
+login.controller('loginController', ['$rootScope', '$scope', '$state', 'AuthService', 'focus', loginController]);
