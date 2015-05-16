015b35aef486481c350fe5702d12df49c4d45242
Nate Moore <ntmoore@knox.edu>
2015-05-16 17:13:54 -0500

    sessionsRead Mode

--- app/js/modules/dashboard/controllers/questionsReadController.js
+++ app/js/modules/dashboard/controllers/questionsReadController.js
@@ -1,3 +1,14 @@
-function questionsReadController($scope) {}
+function questionsReadController($scope, $filter, focus, question, Question, Answer, $stateParams) {
+    $scope.index = $stateParams.index;
 
-dashboard.controller('questionsReadController', ['$scope', questionsReadController]);
+    if (question == undefined) {
+        focus('questionName');
+    } else {
+        $scope.question = question;
+    }
+
+    $scope.question.answers = $filter('orderBy')($scope.question.answers, 'order', false);
+
+}
+
+dashboard.controller('questionsReadController', ['$scope', '$filter', 'focus', 'question', 'Question', 'Answer', '$stateParams', questionsReadController]);
--- app/js/modules/dashboard/controllers/sessionsReadController.js
+++ app/js/modules/dashboard/controllers/sessionsReadController.js
@@ -15,6 +15,57 @@ function sessionsReadController($rootScope, $scope, $filter, focus, screenmatch,
             questionId: $scope.questions[0].id
         })
     }
+
+    $scope.canPrev = false;
+    $scope.canNext = false;
+
+    $rootScope.$on('$stateChangeSuccess',
+        function(event, toState, toParams, fromState, fromParams) {
+            var index = toParams.index;
+
+            if (index == 0) {
+                $scope.canPrev = false;
+                if ($scope.questions.length - 1 == 0) {
+                    $scope.canNext = false;
+                } else {
+                    $scope.canNext = true;
+                }
+            } else if (index == $scope.questions.length - 1) {
+                $scope.canPrev = true;
+                $scope.canNext = false;
+            } else {
+                $scope.canPrev = true;
+                $scope.canNext = true;
+            }
+        });
+
+    $scope.goToPrevQuestion = function() {
+        var index = $state.params.index;
+
+        if (index > 0) {
+            $state.go('questionsRead', {
+                index: index - 1,
+                questionId: $scope.questions[index - 1].id
+            });
+            focus('question-thumb-' + $scope.questions[index - 1].order);
+        } else {
+            return
+        }
+    }
+
+    $scope.goToNextQuestion = function() {
+        var index = $state.params.index;
+        if (index < $scope.questions.length - 1) {
+            $state.go('questionsRead', {
+                index: index + 1,
+                questionId: $scope.questions[index + 1].id
+            });
+
+            focus('question-thumb-' + $scope.questions[index + 1].order);
+        } else {
+            return
+        }
+    }
 }
 
 dashboard.controller('sessionsReadController', ['$rootScope', '$scope', '$filter', 'focus', 'screenmatch', 'course', 'Session', 'session', 'questions', 'Question', '$state', '$stateParams', sessionsReadController]);
